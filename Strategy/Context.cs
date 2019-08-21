74698ec2780d6fe854c2adb612ecff838ed0757f
Maria McDorman <32907456+maria-mcdorman@users.noreply.github.com>
2019-08-21 12:55:04 -0500

    check in strategy pattern

---
+++ Strategy/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Strategy
+{
+    class Context
+    {
+        private Strategy _strategy;
+        public Context(Strategy strategy)
+        {
+            this._strategy = strategy;
+        }
+        public int ExecuteStrategy(int num1, int num2)
+        {
+            return _strategy.DoOperation(num1, num2);
+        }
+    }
+}
